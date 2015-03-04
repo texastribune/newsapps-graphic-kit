4edbdd35c6a2176beaa004f0ae9e160a511d34ec
Jolie McCullough <jmccullough@texastribune.org>
2015-03-04 14:03:46 -0600

    tweaked so it would look good as embed. added some photos

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,13 +1,22 @@
-/* global pym */
+/* global pym, createStoryJS */
 
 (function() {
+
   'use strict';
 
   var pymChild;
 
   function render() {
 
-    // Add your JS here!
+    createStoryJS({
+      type:       'timeline',
+      width:      '100%',
+      height:     '450',
+      source:     'https://docs.google.com/spreadsheet/pub?key=14nkQ9sICq_6r2itJZJhWQZpLHujucQ_ZEtsQZqGspH4&single=true&gid=0&output=html',
+      embed_id:   'timeline-embed',
+      css:        'http://cdn.knightlab.com/libs/timeline/latest/css/timeline.css',
+      js:         'http://cdn.knightlab.com/libs/timeline/latest/js/timeline-min.js'
+    });
 
     if (pymChild) {
       pymChild.sendHeight();
