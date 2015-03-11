cfcf1852a94f5f9429bf22b2fa8c11d30572a3d1
Ryan Murphy <github@accounts.rdmurphy.org>
2015-03-10 23:46:28 -0500

    Port over some new ways of doing things from apps generator

--- gulpfile.js
+++ gulpfile.js
@@ -17,40 +17,49 @@ var browserSync = require('browser-sync');
 var reload = browserSync.reload;
 
 gulp.task('jshint', function() {
-  return gulp.src('app/scripts/**/*.js')
+  return gulp.src(['app/scripts/**/*.js', '!app/scripts/libs/*'])
     .pipe($.jshint())
     .pipe($.jshint.reporter('jshint-stylish'))
     .pipe($.if(!browserSync.active, $.jshint.reporter('fail')));
 });
 
-gulp.task('styles', function() {
+gulp.task('styles', function () {
   return $.sass('app/styles/', {
-      loadPath: ['.'],
-      precision: 10,
-      sourcemap: true
-    })
-    .on('error', console.error.bind(console))
-    .pipe($.autoprefixer({
-      browsers: ['last 2 versions', 'IE 9', 'IE 8'],
-      cascade: false
-    }))
-    .pipe($.sourcemaps.write('.'))
-    .pipe(gulp.dest('.tmp/styles'))
-    .pipe($.size({title: 'styles'}));
+    loadPath: ['.'],
+    precision: 10,
+    sourcemap: true
+  })
+  .on('error', function(err) {
+    console.error('Error', err.message);
+  })
+  .pipe($.autoprefixer({browsers: ['last 2 versions', 'IE 9', 'IE 8']}))
+  .pipe($.sourcemaps.write())
+  .pipe(gulp.dest('.tmp/styles'))
+  .pipe($.if('*.css', reload({stream: true})))
+  .pipe($.size({title: 'styles'}));
 });
 
 gulp.task('templates', function() {
+  var nunjucks = require('nunjucks');
+  var map = require('vinyl-map');
+
   var data = JSON.parse(fs.readFileSync('data.json', 'utf8'));
-  var packageData = JSON.parse(fs.readFileSync('package.json', 'utf8'));
 
   // pulls over the bucket and slug data for the preview page
+  var packageData = JSON.parse(fs.readFileSync('package.json', 'utf8'));
   data.INTERNAL = packageData.config;
 
-  $.nunjucks.nunjucks.configure(['app/']);
+  // disable watching or it'll hang forever
+  nunjucks.configure('app', {watch: false});
 
-  return gulp.src(['app/**/*.html', '!app/includes/*'])
-    .pipe($.nunjucks(data))
-    .pipe(gulp.dest('.tmp'));
+  var nunjuckified = map(function(code, filename) {
+    return nunjucks.renderString(code.toString(), data);
+  });
+
+  return gulp.src(['app/**/{*,!_*}.html', '!app/**/_*.html'])
+    .pipe(nunjuckified)
+    .pipe(gulp.dest('.tmp'))
+    .pipe($.size({title: 'html'}));
 });
 
 gulp.task('images', function() {
